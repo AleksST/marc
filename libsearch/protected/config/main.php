0338d6e1048a63cef3c2b38a6d0c3991f065ac6c
Alex Tushin <aleks.st.rus@gmail.com>
2014-09-12 02:57:38 +0400

    get yaz servers from db

--- libsearch/protected/config/main.php
+++ libsearch/protected/config/main.php
@@ -24,14 +24,14 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'test',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+
 	),
 
 	// application components
@@ -41,29 +41,24 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
+
 		'urlManager'=>array(
-			'urlFormat'=>'path',
+			'urlFormat'=>'get',
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
+
 		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=lib',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'pass',
 			'charset' => 'utf8',
 		),
-		*/
+
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
--- libsearch/protected/controllers/SiteController.php
+++ libsearch/protected/controllers/SiteController.php
@@ -2,7 +2,7 @@
 
 class SiteController extends AppController
 {
-    private $source =  [
+    private $source = [
         'host' => '193.233.14.5',
         'port' => '9999',
         'database' => 'katb',
@@ -12,24 +12,27 @@ class SiteController extends AppController
 
     public function actionIndex() {
         /** @var Rusmarc $rm */
-//        $rm = Marc::factory('rusmarc');
-//        $rm->setRecordsLimit(1);
-//		$request = new RpnQuery;
-//		$request->addAnywhereCondition('любовь');
-//		$rm->parseZServer($this->source, $request);
-//        $this->render('index', ['marc' => $rm]);
         $this->render('index');
     }
 
     public function actionSearch() {
-        $search = Yii::app()->request->getQuery('q', null);
-        /** @var Rusmarc $rm */
-        $rm = Marc::factory('rusmarc');
-        $rm->setRecordsLimit(5);
-        $request = new RpnQuery;
-        $request->addAnywhereCondition($search);
-        $rm->parseZServer($this->source, $request);
-        echo $rm->getRecordsJson();
+		$search = Yii::app()->request->getQuery('q', null);
+		foreach (Zserver::model()->findAll() as $resource) {
+			$data = [
+				'host' => $resource->host,
+				'port' => $resource->port,
+				'database' => $resource->db,
+				'charset' => 'windows-1251',
+				'syntax' => 'rusmarc',
+			];
+			/** @var Rusmarc $rm */
+			$rm = Marc::factory('rusmarc');
+			$rm->setRecordsLimit(5);
+			$request = new RpnQuery;
+			$request->addAnywhereCondition($search);
+			$rm->parseZServer($data, $request);
+			echo $rm->getRecordsJson();
+		}
         Yii::app()->end();
     }
 }
